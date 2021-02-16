a82966bbbfae4f93ba360095294e0e3f019ca319
Patricio Morel <totenbot@outlook.com>
2021-02-16 18:27:30 -0300

    Disable dragging covered cards

--- src/components/CrCard.js
+++ src/components/CrCard.js
@@ -3,20 +3,19 @@ export default class CrCard extends HTMLElement {
     static PLACEHOLDER = document.createElement('div')
     constructor(){
         super()
-        this.setAttribute('draggable', 'true')
+        if(!this.classList.contains('covered')){
+            this.setAttribute('draggable', 'true')
+        }
         this.addEventListener('dragstart', this.dragStart.bind(this))
         this.addEventListener('dragenter', this.dragEnter.bind(this))
-        // this.addEventListener('dragleave', this.dragLeave.bind(this))
         this.addEventListener('drop', this.drop.bind(this))
 
         this.placeholder = null
     }
     dragStart(e){
         e.dataTransfer.setData('text/plain', this.id)
-        console.log('dragged')
     }
     dragEnter(e){
-        // e.preventDefault()
         e.stopPropagation()
         this.parentElement.insertBefore(CrCard.PLACEHOLDER,this)
     }
